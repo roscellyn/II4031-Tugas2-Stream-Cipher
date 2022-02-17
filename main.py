e01ed9172756b9c118330352f4bb35cc5266fde9
Edwin Stanic <18219079@std.stei.itb.ac.id>
2022-02-17 21:49:42 +0700

    add comment

--- main.py
+++ main.py
@@ -1,169 +1,3 @@
-# S = [0 for i in range (256)]
-
-# # Key-Scheduling Algorithm (KSA)
-# # Inisialisasi array S dengan bilangan genap lalu diikuti bilangan ganjil
-# for i in range(128):
-#     self.S[i] = i*2
-
-# for i in range(128, 256):
-#     self.S[i] = 1 + (i-128)*2
-
-# # Modifikasi LFSR
-# # Inisialisasi LFSR menggunakan kunci masukan pengguna
-# K = input()
-# KinB = bytes(K,'utf-8')
-# arr = []
-# for byte in KinB:
-#     arr.append(byte)
-#     # print(byte)
-# Ks = []
-# for i in range(len(arr)):
-#     temp = arr[len(arr)-1] ^ arr[0]
-#     Ks.append(arr[0])
-    
-#     for j in range(len(arr)-1):
-#         arr[j] = arr[j+1]
-#     arr[len(arr)-1] = (temp)
-#     # print(arr)
-
-# # print("Final KinB: " + str(arr))
-
-# j = 0
-# for i in range(256):
-#     j = (j + self.S[i] + arr[i % len(arr)]) % 256
-    
-#     # Swap
-#     temp = self.S[i]
-#     self.S[i] = self.S[j]
-#     self.S[j] = temp
-
-# SC = []
-# for i in S:
-#     SC.append(i)
-
-# # Enkripsi
-# def encrypt(plainteks, is_file, filename):
-#     # Pseudo-random Generation Algorithm (PRGA)
-#     if(is_file):
-#         f=open(filename,"rb")
-#         file_bytes = f.read()
-#         f.close()
-#         PinB = file_bytes
-#     else:
-#         P = plainteks
-#         PinB = bytes(P,'utf-8')
-#     # print(bytes)
-#     # for byte in bytes:
-#     #     print(bytes
-    
-#     arrP = []
-#     for byte in PinB:
-#         arrP.append(byte)
-
-#     arrC = []
-#     strC = []
-#     i = 0
-#     j = 0
-
-#     for idx in range(len(arrP)):
-#         i = (i + 1) % 256
-#         j = (j + self.S[i]) % 256
-        
-#         # Swap
-#         temp = self.S[i]
-#         self.S[i] = self.S[j]
-#         self.S[j] = temp
-
-#         t = (self.S[i] * self.S[j]) % 256
-#         u = S[t]
-#         C = u ^ arrP[idx]
-#         # print("Tipe C: " , type(C))
-#         # print("Tipe u: " , type(u))
-#         # print("Tipe arrP : " , type(arrP[idx]))
-#         # C = bytes(str(C),'utf-8')
-#         # print(C)
-#         strC.append(chr(C))
-#         arrC.append(C)
-
-#     if(is_file):
-#         arrCB = bytearray(arrC)    
-#         # print("Ciperteks= " , ''.join(strC))
-        
-#         w=open("encrypt-results.pdf", "wb")
-#         w.write(arrCB)
-        
-#         return arrCB
-
-#     return arrC
-
-# # Dekripsi
-# def decrypt(arrC):
-#     # Pseudo-random Generation Algorithm (PRGA)
-#     # print(arrC)
-#     # CinB = bytes(C, 'utf-8')
-#     # arrC = []
-#     # for byte in CinB:
-#     #     arrC.append(byte)
-
-#     # print(type(arrC))
-#     is_file = False
-    
-#     if(type(arrC) is bytearray):
-#         is_file = True
-#         temp = []
-#         for byte in arrC:
-#             temp.append(byte)
-#         arrC = temp
-
-#     arrP = []
-#     arrPB = []
-#     i = 0
-#     j = 0
-
-#     # print(CinB)
-    
-#     for idx in range(len(arrC)):
-#         i = (i + 1) % 256
-#         j = (j + SC[i]) % 256
-        
-#         # Swap
-#         temp = SC[i]
-#         SC[i] = SC[j]
-#         SC[j] = temp
-
-#         t = (SC[i] * SC[j]) % 256
-#         u = SC[t]
-#         P = u ^ arrC[idx]
-#         # print("Tipe C: " , type(C))
-#         # print("Tipe u: " , type(u))
-#         # print("Tipe arrP : " , type(arrP[idx]))
-#         # C = bytes(str(C),'utf-8')
-#         # print(C)
-#         arrPB.append(P)
-#         arrP.append(chr(P))
-    
-#     # print(is_file)
-    
-#     if(is_file):
-#         arrPB = bytearray(arrPB)  
-#         w=open("decrypt-results.pdf", "wb")
-#         w.write(arrPB)
-#     else:
-#         print("Plainteks= " ,''.join(arrP))
-
-# is_file = False
-# filename = ""
-# plainteks = ""
-# choice = input("Pilih plainteks(A) atau upload file(B)? ")
-# if(choice == "B"):
-#     is_file = True
-#     filename = input("Masukkan nama file: ")
-# else:
-#     plainteks = input("Plainteks: ")
-    
-# cipherteks = encrypt(plainteks, is_file, filename)
-# decrypt(cipherteks)
-
 # ! /usr/bin/python3
 from sys import exit as sysExit
 import sys
@@ -298,10 +132,16 @@ class MainScreen(QtWidgets.QMainWindow):
         for i in range(128, 256):
             self.S[i] = 1 + (i-128)*2
 
-        # Modifikasi LFSR
-        # Inisialisasi LFSR menggunakan kunci masukan pengguna
+        # Menambahkan LFSR yang dimodifikasi untuk menghasilkan keystream
+        """
+            1. Inisialisasi LFSR menggunakan kunci masukan pengguna
+            2. Bit paling kiri di-XOR dengan bit paling kanan
+            3. Keystream diambil dari bit paling kiri yang akan digeser keluar
+            4. Menggeser setiap bit ke kiri
+            5. Hasil XOR akan dimasukkan ke bit paling kanan
+        """
         K = self.key
-        KinB = bytes(K,'utf-8')
+        KinB = bytes(K,'latin-1')
         arr = []
         for byte in KinB:
             arr.append(byte)
@@ -315,6 +155,7 @@ class MainScreen(QtWidgets.QMainWindow):
                 arr[j] = arr[j+1]
             arr[len(arr)-1] = (temp)
 
+        # Permutasi elemen array S berdasarkan kunci K
         j = 0
         for i in range(256):
             j = (j + self.S[i] + arr[i % len(arr)]) % 256
@@ -326,7 +167,6 @@ class MainScreen(QtWidgets.QMainWindow):
 
     def encrypt(self):
         self.generate_key()
-        # Pseudo-random Generation Algorithm (PRGA)
         if(self.is_file):
             f=open(self.filename,"rb")
             file_bytes = f.read()
@@ -334,7 +174,7 @@ class MainScreen(QtWidgets.QMainWindow):
             PinB = file_bytes
         else:
             P = self.input_text
-            PinB = bytes(P,'utf-8')
+            PinB = bytes(P,'latin-1')
         
         arrP = []
         for byte in PinB:
@@ -342,6 +182,12 @@ class MainScreen(QtWidgets.QMainWindow):
 
         arrC = []
         strC = []
+
+        # Pseudo-random Generation Algorithm (PRGA)
+        """
+            PRGA dimodifikasi pada bagian penentuan indeks keystream
+            yang didapatkan dengan S[i] * S[j] 
+        """
         i = 0
         j = 0
 
@@ -361,12 +207,10 @@ class MainScreen(QtWidgets.QMainWindow):
             arrC.append(C)
 
         self.arrCB = bytearray(arrC)
-        
         self.output.setText(''.join(strC))
 
     def decrypt(self):
         self.generate_key()
-        # Pseudo-random Generation Algorithm (PRGA)
         if(self.is_file):
             f=open(self.filename,"rb")
             file_bytes = f.read()
@@ -375,7 +219,7 @@ class MainScreen(QtWidgets.QMainWindow):
         else:
             C = self.input_text
             print(C)
-            CinB = bytes(C,'utf-8')
+            CinB = bytes(C,"latin-1")
         
         arrC = []
         for byte in CinB:
@@ -383,6 +227,12 @@ class MainScreen(QtWidgets.QMainWindow):
 
         arrP = []
         strP = []
+
+        # Pseudo-random Generation Algorithm (PRGA)
+        """
+            PRGA dimodifikasi pada bagian penentuan indeks keystream
+            yang didapatkan dengan S[i] * S[j] 
+        """
         i = 0
         j = 0
         
